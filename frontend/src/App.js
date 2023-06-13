f437a59860e1a59f6987fa60da901fe489468871
Hanaa Sadoqi <40529421+hsadoqi@users.noreply.github.com>
2023-06-12 22:24:16 -0400

    Create files for backend and frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css';
+import ShoppingListContainer from './containers/ShoppingListContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello World!
+        <ShoppingListContainer/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/containers/ShoppingListContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function ShoppingListContainer(){
+    return (
+        <div id="shopping-list-container">
+            ShoppingListContainer
+        </div>
+    )
+}
+
+export default ShoppingListContainer;
